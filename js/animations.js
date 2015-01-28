74192042ffad0213d8525310e78ccc9e89ff5624
lrobins <lrobins@adobe.com>
2015-01-28 14:25:22 -0700

    this adds the character counter and the start of the prepend tweet

--- js/animations.js
+++ js/animations.js
@@ -1,20 +1,53 @@
-
-
  $(document).ready(function() {
 
+ 	//increase tweet field height
 	$('.tweet-compose').on('focus', function(){
-		$('.tweet-compose').css('height', '66px');
+		$(this).css('height', '66px');
 		$('#tweet-controls').css('display', 'block');
-		$
 	});
+
+	//decrease tweet field height
 	$('.tweet-compose').on('blur', function(){
 		$('.tweet-compose').css('height', '33px');
 	});
 
-	$('.tweet-compose').on('keypress', function() {
-		$('#char-count').val();
+	//Character counter
+	$('.tweet-compose').keypress(function() { 
+    var tweetLength = $('.tweet-compose').val().length; 
+	var tweetCount = 140 - tweetLength;
+	$('#char-count').html(tweetCount);
+    if(tweetCount <= 10) {
+        $('#char-count').css({'color' : 'red'})
+    }
+    else {
+        $('#char-count').css({'color' : '#999'})
+    }
+    if(tweetCount < 0) {
+        $('.button').prop('disabled', true); 
+    } else {
+        $('.button').prop('disabled', false);
+    }
+ 	});
+
+	//Prepend new Tweet
+	$('#tweet-submit').on('click', function(){
+		var tweetMessage = $('.tweet-compose').val();
+		$('.tweet').prepend(tweetMessage);
 	});
- 	
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
  });
