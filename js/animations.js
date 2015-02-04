b78d484fea900002451695d9252fee02a20bb80f
lrobins <lrobins@adobe.com>
2015-02-03 20:04:33 -0700

    adds completed project using jQuery

--- js/animations.js
+++ js/animations.js
@@ -12,7 +12,7 @@
 	});
 
 	//Character counter
-	$('.tweet-compose').keypress(function() { 
+	$('.tweet-compose').on('keydown, keypress, keyup', function() { 
     var tweetLength = $('.tweet-compose').val().length; 
 	var tweetCount = 140 - tweetLength;
 	$('#char-count').html(tweetCount);
@@ -31,18 +31,45 @@
 
 	//Prepend new Tweet
 	$('#tweet-submit').on('click', function(){
+	if($('.tweet-compose').val().length !== 0) {
 		var tweetMessage = $('.tweet-compose').val();
-		$('.tweet').prepend(tweetMessage);
+		//html stuff to inject:
+		var htmlInject = "<div class=\"tweet\"><div class=\"content\"><img class=\"avatar\" src=\"img/alagoon.jpg\"/><strong class=\"fullname\">Logan Robins </strong><span class=\"username\">@LxRx</span><p class=\"tweet-text\">" + tweetMessage + "</p><div class=\"tweet-actions\"><ul><li><span class=\"icon action-reply\"></span> Reply</li><li><span class=\"icon action-retweet\"></span> Retweet</li><li><span class=\"icon action-favorite\"></span> Favorite</li><li><span class=\"icon action-more\"></span> More</li></ul></div><div class=\"stats\"><div class=\"retweets\"><p class=\"num-retweets\">12</p><p>RETWEETS</p></div><div class=\"favorites\"><p class=\"num-favorites\">1</p><p>FAVORITES</p></div><div class=\"users-interact\"></div><div class=\"time\">1:04 PM - 19 Sep 13</div></div><div class=\"reply\"><img class=\"avatar\" src=\"img/alagoon.jpg\"/></div></div></div>";
+		$('#stream').prepend(htmlInject);
+		$(".tweet-compose").val(""); // makes the form an empty string after the submit click
+		$('#char-count').text('140'); // resets counter to 140 after submit
+	} else {
+	alert("Please enter some text to tweet");
+}
+});
+
+
+/*
+	//Reveal Stats on Tweet Click
+	$('.tweet').on('click', '.stats', function() {  //this revealed ALL stats on click
+		$('.stats').toggle();
+		$('.reply').toggle();
+		return false;
 	});
+*/
 
 
+$('.stats').hide();
+$('.reply').hide();
+$('.tweet').click(function() { //hides tweet stats and reply when you click on another tweet
+    $('.stats').hide();
+    $('.reply').hide();
+    $(this).find('.stats').show();
+    $(this).find('.reply').show();
+});
 
-
-
-
-
-
-
+$(document).click(function(event) {  //hides tweet stats and reply when you click outside of any tweet.
+    if(!$(event.target).closest('.tweet').length) { 
+        $('.stats').hide();
+        $('.reply').hide();
+        
+    }        
+});
 
 
 
